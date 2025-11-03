05b429137d38e0fff2aaba2c1a6c7cf5b88d22b4
guerrerosuarezd <guerrerosuarezd@wit.edu>
2025-11-03 14:13:34 -0500

    Completed Part A

--- parta.c
+++ parta.c
@@ -1,8 +1,27 @@
 #include <stdbool.h>
 #include <stddef.h>
+#include <stdio.h>
+#include <ctype.h>
 
-// TODO: Include more header files (read the manpages to find out more!)
-
+// A simple echo-like program that uppercases all provided arguments and
+// prints them separated by commas. If no arguments are provided an error is
+// printed and the program exits with status 1.
 int main(int argc, const char* argv[]) {
+    if (argc <= 1) {
+        puts("ERROR: No arguments");
+        return 1;
+    }
+
+    // Iterate over arguments (skip argv[0])
+    for (int i = 1; i < argc; ++i) {
+        const char* s = argv[i];
+        // Print each character upper-cased
+        for (const char* p = s; *p != '\0'; ++p) {
+            putchar((int)toupper((unsigned char)*p));
+        }
+        // Print comma between arguments (no trailing comma at end)
+        if (i < argc - 1) putchar(',');
+    }
+
     return 0;
 }
